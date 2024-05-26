cd100a6fcace7436bc8892e069a589aafa9a8c4e
DusanZigic <zigic.dusan1991@gmail.com>
2024-05-26 15:15:35 +0200

    amended run_eloss.py

--- utils/run_eloss.py
+++ utils/run_eloss.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
-from sys import argv, exit
 from os import path
-from subprocess import Popen, call
+from subprocess import Popen
 import numpy as np
 import json
 
-#########################################################################################################################################
 def integrateRAA(pname):
-	raadist   = np.loadtxt('{0:s}.dat'.format(pname))
+# function that integrates R_AA(pT, phi) over phi
+	raadist   = np.loadtxt(f"{pname:s}.dat")
 	ptpts	  = np.sort(np.unique(raadist[:,0]))
 	phipts	  = np.sort(np.unique(raadist[:,1]))
 	raa       = np.zeros(ptpts.shape[0])
@@ -20,59 +19,58 @@ def integrateRAA(pname):
 		raa[ipt] = norm/2.0/np.pi
 		v2[ipt]  = np.sum(gqweights*raadist[ipt]*np.cos(2.0*phipts))/norm
 	return np.column_stack((ptpts, raa, v2))
-#########################################################################################################################################
 
-#########################################################################################################################################
 def exportOBS(pname, obs):
+# function that exports observables: R_AA(pT) and v_2(pT)
 	header = []
-	with open('{0:s}.dat'.format(pname), 'r') as f:
+	with open(f"{pname:s}.dat", 'r') as f:
 		while True:
 			line = f.readline().rstrip()
 			if line[0] == '#': header.append(line)
 			else: break
-	p_name = {'bottom': 'b', 'charm': 'd', 'chargedhadrons': 'ch'}
+	p_name = {"bottom": "b", "charm": "d", "chargedhadrons": "ch"}
 	header[0] = header[0].replace(pname, p_name[pname])
-	header[7] = header[7].replace('R_AA', 'v_2')
-	header[7] = header[7].replace('phi',  'R_AA')
-	with open('{0:s}.dat'.format(p_name[pname]), 'w') as f:
-		for h in header: f.write(h + '\n')
-		for x in obs: f.write('{0:14.10f}  {1:12.10f}  {2:12.10f}\n'.format(x[0], x[1], x[2]))
-#########################################################################################################################################
+	header[7] = header[7].replace("R_AA", "v_2")
+	header[7] = header[7].replace("phi",  "R_AA")
+	with open(f"{p_name[pname]}.dat", 'w') as f:
+		for h in header:
+			f.write(f"{h}\n")
+		for x in obs:
+			f.write(f"{x[0]:14.10f}  {x[1]:12.10f}  {x[2]:12.10f}\n")
 
-#########################################################################################################################################
 if __name__ == '__main__':
 
-	main_dir = path.abspath('')
+	main_dir = path.abspath("")
 
-	with open('params.json', 'r') as jsonf: params = json.load(jsonf)
+	with open("params.json", 'r') as jsonf: params = json.load(jsonf)
 
 	numthreads = [len([*range(params['dreena']['NUM_THREADS'])][i::3]) for i in range(3)]
 
-	commandStr  = 'export OMP_NUM_THREADS={0:d}; '.format(numthreads[2])
-	commandStr += './DREENAA AverageEL --c=dreena.conf --pName=Bottom; '
-	commandStr += './DREENAA AverageEL --c=dreena.conf --pName=Charm;'
-	hf_command  = Popen(commandStr, shell=True, cwd=main_dir)
+	commandStr  = f"export OMP_NUM_THREADS={numthreads[2]:d}; "
+	commandStr += "./DREENAA AverageEL --c=dreena.conf --pName=Bottom; "
+	commandStr += "./DREENAA AverageEL --c=dreena.conf --pName=Charm;"
+	heavyFlavourCommand  = Popen(commandStr, shell=True, cwd=main_dir)
 
-	commandStr  = 'export OMP_NUM_THREADS={0:d}; ./DREENAA AverageEL --c=dreena.conf --pName=LQuarks;'.format(numthreads[0])
-	lq_command  = Popen(commandStr, shell=True, cwd=main_dir)
+	commandStr  = f"export OMP_NUM_THREADS={numthreads[0]:d}; ./DREENAA AverageEL --c=dreena.conf --pName=LQuarks;"
+	lightQuarksCommand  = Popen(commandStr, shell=True, cwd=main_dir)
 
-	commandStr  = 'export OMP_NUM_THREADS={0:d}; ./DREENAA AverageEL --c=dreena.conf --pName=Gluon;'.format(numthreads[1])
-	gl_command  = Popen(commandStr, shell=True, cwd=main_dir)
+	commandStr  = f"export OMP_NUM_THREADS={numthreads[1]:d}; ./DREENAA AverageEL --c=dreena.conf --pName=Gluon;"
+	gluonCommand  = Popen(commandStr, shell=True, cwd=main_dir)
 
-	lq_command.wait()
-	gl_command.wait()
+	lightQuarksCommand.wait()
+	gluonCommand.wait()
 
-	commandStr  = 'export OMP_NUM_THREADS={0:d}; ./DSSFFs --c=dssffs.conf;'.format(params['dreena']['NUM_THREADS'])
-	ch_command  = Popen(commandStr, shell=True, cwd=main_dir)
+	# commandStr  = "export OMP_NUM_THREADS={0:d}; ./DSSFFs --c=dssffs.conf;".format(params['dreena']['NUM_THREADS'])
+	# chargedHadronsCommand  = Popen(commandStr, shell=True, cwd=main_dir)
 
-	hf_command.wait()
-	ch_command.wait()
+	heavyFlavourCommand.wait()
+	# chargedHadronsCommand.wait()
 
-	obs = integrateRAA('bottom')
-	exportOBS('bottom', obs)
+	obs = integrateRAA("bottom")
+	exportOBS("bottom", obs)
 
-	obs = integrateRAA('charm')
-	exportOBS('charm', obs)
+	obs = integrateRAA("charm")
+	exportOBS("charm", obs)
 
-	obs = integrateRAA('chargedhadrons')
-	exportOBS('chargedhadrons', obs)
+	# obs = integrateRAA("chargedhadrons")
+	# exportOBS("chargedhadrons", obs)
